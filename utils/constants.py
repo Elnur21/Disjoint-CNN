5e887b27dd996326e5ca7f5fe1f48008eb84b77d
Elnur21 <elnurme@code.edu.az>
2024-03-15 17:12:16 +0100

    write datasets names

---
+++ utils/constants.py
@@ -0,0 +1,6 @@
+datasets = ["ArticularyWordRecognition","AtrialFibrillation","BasicMotions","CharacterTrajectories","Cricket",
+            "DuckDuckGeese","EigenWorms", "Epilepsy","ERing","EthanolConcentration","FaceDetection","FingerMovements",
+            "HandMovementDirection","Handwriting","Heartbeat","InsectWingbeat","JapaneseVowels",
+            "Libras","LSST","MotorImagery","NATOPS","PEMS-SF","PenDigits","PhonemeSpectra"
+            "RacketSports", "SelfRegulationSCP1","SelfRegulationSCP2","SpokenArabicDigits",
+            "StandWalkJump","UWaveGestureLibrary"]
